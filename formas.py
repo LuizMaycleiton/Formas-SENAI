8be6249fef6f9f0494964bc68a8d10198d645119
LUIZ MAYCLEITON GOMES DE SOUSA <26211012@dsn127m1241953.cfp127aluno.local>
2026-09-18 13:38:12 -0300

    Adicionando a Func Circulo

--- formas.py
+++ formas.py
@@ -6,3 +6,41 @@
 # 5 - Paralelograma
 # 6 - LOSANGO
 # 7 - TRAPÉZIO
+
+def circulo():
+    pi = float(input("Digite o valor de PI: "))
+    raio = float(input("Digite o valor do Raio: ")) 
+    conta_raio = raio * raio
+    conta_circulo = pi * conta_raio 
+    print(conta_circulo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+while True:
+    print("Áreas das Formas")
+    print("1 - Círculo")
+    print("2 - Triângulo")
+    print("3 - Quadrado")
+    print("4 - Retângulo")
+    print("5 - Paralelograma")
+    print("6 - Losango")
+    print("7 - Trapézio")
+
+    opcao = input("Escolha uma opcão: ")
+
+    if opcao == "1":
+        circulo()
